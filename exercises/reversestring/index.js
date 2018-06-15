5897ecd60204bdc3d12fb084381f24df9fbc7546
Manisha11sep <er.manisha11sep@gmail.com>
2018-06-15 13:47:35 -0700

    soulution 2 using for loop

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,9 +6,21 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {
-  return str.split("").reverse().join("");
+// function reverse(str) {
+//   return str.split("").reverse().join("");
 
+// }
+
+
+//Using for loop to reverse a string starting from the end of string and storing it inside a new String
+function reverse(str) {
+    var newStr = ""
+
+    
+for(let i = str.length-1; i>=0; i--){
+    newStr += str[i];
 }
+  return newStr;
+  }
 
 module.exports = reverse;
