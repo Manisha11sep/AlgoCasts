09e8baac59fd3e299d3c0cbd54626986ee8f0971
Manisha11sep <er.manisha11sep@gmail.com>
2018-06-16 10:26:17 -0700

    array chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -7,7 +7,17 @@
 // chunk([1, 2, 3, 4, 5, 6, 7, 8], 3) --> [[ 1, 2, 3], [4, 5, 6], [7, 8]]
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
-
-function chunk(array, size) {}
-
+function chunk(array,size){
+    let newArr=[]
+    let i=0;
+    while(i<array.length){
+      console.log("value of i",i)
+      const spliced = array.slice(i,i+size)
+       console.log(spliced)
+      newArr.push(spliced)
+      i+=size;
+    }
+  return newArr
+  }
+  
 module.exports = chunk;
