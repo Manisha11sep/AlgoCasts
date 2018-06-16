8c8c76a332641ca5db19d564b3bf9b90154788ba
Manisha11sep <er.manisha11sep@gmail.com>
2018-06-15 20:46:04 -0700

    Counting values of characters in string

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -4,7 +4,26 @@
 // --- Examples
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
-
-function maxChar(str) {}
+function maxChar(str){
+    map={}
+    max=0;
+    maxChar=''
+    for(char of str){
+  
+      if(!map[char]){
+        map[char]=1
+      }
+      else
+      map[char]++
+    }
+    console.log(map)
+    for(let char in map){
+      if(map[char]>max){
+        max=map[char];
+        maxChar=char
+      }
+    }
+    return maxChar
+  }
 
 module.exports = maxChar;
