61083307622d10993cfb80b841102b12f6541d1c
Manisha11sep <er.manisha11sep@gmail.com>
2018-06-16 12:35:37 -0700

    capitalization

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -6,7 +6,13 @@
 //   capitalize('a short sentence') --> 'A Short Sentence'
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
-
-function capitalize(str) {}
-
+function capitalize(str) {
+    const newArr =[];
+   const capital= str.split(" ")
+   console.log(capital)
+   for(let word of capital){
+  newArr.push(word[0].toUpperCase().concat(word.slice(1)))
+   }
+  return newArr.join(" ");
+  }
 module.exports = capitalize;
